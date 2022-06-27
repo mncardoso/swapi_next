04451c73268dcf5edf9e519910e6be97f2d4afd6
Miguel Cardoso <6021036+mncardoso@users.noreply.github.com>
2022-06-27 19:07:13 +0100

    done

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
-
-module.exports = nextConfig
+module.exports = {
+	reactStrictMode: true,
+	experimental: {
+		images: {
+			layoutRaw: true,
+		},
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: ["style-loader", "postcss-loader"],
+			},
+			{
+				test: /\.jsx?$/,
+				use: ["babel-loader", "astroturf/loader"],
+			},
+		],
+	},
+};
